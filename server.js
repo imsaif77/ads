cb1c9ce566194706f08ad5726922fb8bb07b5c8d
smartflutter <98512824+smartflutter@users.noreply.github.com>
2022-04-09 13:20:10 +0530

    make it better

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const server = express()
+const body_parser = require('body-parser')
+
+const { port } = require('./config');
+
+const app = require('./app');
+
+server.use(body_parser.json())
+
+
+
+app.listen(port, () => {
+  console.log(`Application is running on port ${port}`);
+});
